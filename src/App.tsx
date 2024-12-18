053d2a3224bde655265008cddf45c5bb40df9928
Jean <jeanmp2116@gmail.com>
2024-12-18 13:54:29 +0100

    creation Todo, Todos components, added styles and remove functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
+import { useState } from "react";
+import { Todos } from "./components/Todos";
 import "./App.css";
+import { TodoId } from "./types";
 
-function App() {
+const mockTodos = [
+  {
+    id: "1",
+    title: "todo 1",
+    completed: true,
+  },
+  {
+    id: "2",
+    title: "todo 2",
+    completed: false,
+  },
+  {
+    id: "3",
+    title: "todo 3",
+    completed: false,
+  },
+];
+
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos);
+
+  const handleRemove = ({id}: TodoId) : void => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-500 text-white">
-      <h1 className="text-3xl">Tailwind with React+TypeScript</h1>
+    <div className="bg-gray-900 text-gray-100 min-h-screen flex items-center justify-center p-6">
+      <div className="w-full max-w-md">
+        <Todos 
+        todos={todos} 
+        onRemoveTodo={handleRemove}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
